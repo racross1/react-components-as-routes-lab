4e6ef5324ef519d7d24fef843b360dcdfae40cc6
racross1 <racross1@gmail.com>
2021-02-09 16:55:41 -0800

    commit tree 1c75f8f64c6c279cc87755a002d10c9836def80b

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(a => {
+        return <div> 
+          {a.name}
+          <ul>
+            {a.movies.map(m => <li>{m}</li>)}
+          </ul>
+        </div>
+        }
+    )}
     </div>
-  );
-};
+  )
+}
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(m => {
+          return <div> 
+            {m.title}
+            <br></br>
+            {m.time}
+            <ul>
+              {m.genres.map(g => <li>{g}</li>)}
+            </ul>
+           
+          </div>
+          })
+        }
     </div>
-  );
+
+);
 };
 
 export default Movies;
+
+
